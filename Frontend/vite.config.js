10163c38ac344d949f5916f65de3b7299f759074
VishwayishwaranTL <vishwayishwaran2210619@gmail.com>
2025-03-10 18:14:11 +0530

    config

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,11 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   base: "./",
   build: {
-    outDir: "dist"
+    outDir: "dist",
+    chunkSizeWarningLimit: 1000, // Increase limit to 1000 KB (default is 500 KB)
   },
   server: {
     historyApiFallback: true
